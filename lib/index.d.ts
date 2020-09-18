ff4cd840400bb5bc97874b660908cb3df991000f
whtoo <whtoo@qq.com>
2020-09-18 14:22:04 +0800

    1. 完成初始化环境配置。

---
+++ lib/index.d.ts
@@ -0,0 +1,15 @@
+
+/**
+ * 返回 hello 开头的字符串
+ * @param str - input string
+ * @returns 'hello xxx'
+ * @example
+ * ```ts
+ * myFirstFunc('ts') => 'hello ts'
+ * ```
+ * @beta
+ */
+declare function myFirstFunc(str: string): string;
+export default myFirstFunc;
+
+export { }
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * 返回 hello 开头的字符串
+ * @param str - input string
+ * @returns 'hello xxx'
+ * @example
+ * ```ts
+ * myFirstFunc('ts') => 'hello ts'
+ * ```
+ * @beta
+ */
+export default function myFirstFunc (str: string) {
+    return `hello ${str}`
+}
+  
