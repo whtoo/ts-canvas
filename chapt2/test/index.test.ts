d9a28b73db5434ff0859a0ddc0bdb9091b21bdac
whtoo <whtoo@qq.com>
2020-09-21 09:16:01 +0800

    1. 设定工程原型模版，实现快速模板复制

---
+++ chapt2/test/index.test.ts
@@ -0,0 +1,15 @@
+// index.test.ts
+
+import assert from 'assert'
+import myFirstFunc from '../src/lib'
+
+describe('validate:', () => {
+  /**
+   * myFirstFunc
+   */
+  describe('myFirstFunc', () => {
+    test(' return hello rollup ', () => {
+      assert.strictEqual(myFirstFunc('rollup'), 'hello rollup')
+    })
+  })
+})
