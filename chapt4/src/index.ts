a7ccb2f741d02b1f353d5940765f0ad8545b1486
whtoo <whtoo@qq.com>
2020-09-21 21:06:43 +0800

    1. 初始化第四章.

---
+++ chapt4/src/index.ts
@@ -0,0 +1,37 @@
+import { Application } from "./application";
+import { CanvasKeyBoardEvent, CanvasMouseEvent } from "./application";
+class ApplicationTest extends Application {
+    protected dispatchKeyDown ( evt : CanvasKeyBoardEvent) : void {
+       console . log ( " key : " + evt.key + " is down " ) ;
+    }
+
+    protected dispatchMouseDown ( evt : CanvasMouseEvent ) : void {
+        console . log ( " canvasPosition : " + evt . canvasPosition ) ;
+    }
+
+    public update ( elapsedMsec : number , intervalSec : number ) : void {
+        console . log ( " elapsedMsec : " + elapsedMsec + " intervalSec : " + intervalSec ) ;
+    }
+
+    public render ( ) : void {
+        console . log ( " 调用render方法 " ) ;
+    }
+}
+
+const canvas : HTMLCanvasElement | null = document . getElementById ( 'canvas' ) as HTMLCanvasElement ;
+
+const app : Application = new ApplicationTest ( canvas ) ;
+
+app . update ( 0 , 0 ) ;
+app . render ( ) ;
+
+const startButton : HTMLButtonElement | null = document . getElementById ( 'start' ) as HTMLButtonElement ;
+const stopButton : HTMLButtonElement | null = document . getElementById ( 'stop' ) as HTMLButtonElement ;
+
+startButton . onclick = ( ev : MouseEvent ) : void => {
+    app . start ( ) ;
+}
+
+stopButton . onclick = ( ev : MouseEvent ) : void => {
+    app . stop ( ) ;
+}
