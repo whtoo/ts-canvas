decc1bac492e809eecb5bd3efcbe041c627915e2
Arthur.Whtoo <whtoo@qq.com>
2021-04-18 11:24:47 +0800

    1. init chapt8.

---
+++ chapt8/src/drawtypes.ts
@@ -0,0 +1,18 @@
+export enum EImageFillType {
+    STRETCH,
+    REPEAT,
+    REPEAT_X,
+    REPEAT_Y
+}
+
+export enum ELayout {
+    LEFT_TOP,
+    RIGHT_TOP,
+    RIGHT_BOTTOM,
+    LEFT_BOTTOM,
+    CENTER_MIDDLE,
+    CENTER_TOP,
+    RIGHT_MIDDLE,
+    CENTER_BOTTOM,
+    LEFT_MIDDLE
+}
